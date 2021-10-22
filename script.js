2f0a927497e00f2f619a12bd75406b7527cba3eb
siracussa <92761006+siracussa@users.noreply.github.com>
2021-10-21 15:09:17 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function save() {
   //get value
   var b = document.getElementById('word').value;
    //clears field
-  document.getElementById('word').value='';\
+  document.getElementById('word').value='';
   window.alert(b);
 }
 
