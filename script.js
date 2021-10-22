6975b5b20ed5039a52217fc33f04643959fa077a
siracussa <92761006+siracussa@users.noreply.github.com>
2021-10-22 11:29:08 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var b;
-function save() {
-  b = document.getElementById('word').value;
-  window.alert(b);
+var a;
+function getName() {
+  a = document.getElementById('username').value;
+  window.alert(a);
 }
 
+var b;
+function getName() {
+  //get value from input field and save as b
+  b = document.getElementById('username').value;
+  //create LS key with value of 'b'
+  localStorage.setItem('uName', b);
+  //automatically take user to page two.html
+  location.href = "two.html";
+}
+
+function getUser() {
+  var c = localStorage.getItem('uName');
+  document.getElementById('placeholder').innerHTML = "Hello" + c;
 
