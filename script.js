ba68e89d8803a8fc5bf820f364b905786c8041af
siracussa <92761006+siracussa@users.noreply.github.com>
2021-10-21 15:07:02 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var a = []
 function save() {
   //get value
   var b = document.getElementById('word').value;
-   a.push(b);
    //clears field
   document.getElementById('word').value='';
 }
- window.alert(a);
+ window.alert(b);
