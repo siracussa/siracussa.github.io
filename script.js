24d047dcdd8b761dbd1eedf287ce4d5b70981547
siracussa <92761006+siracussa@users.noreply.github.com>
2021-10-25 12:12:28 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 function getName() {
   a = document.getElementById('username').value;
   window.alert(a);
-}*/
-
-var b;
+}
+/*
+var fieldValue;
 function getName() {
   //get value from input field and save as b
-  b = document.getElementById('username').value;
+  fieldValue = document.getElementById('username').value;
   document.getElementById("username").value="";
-  //create LS key with value of 'b'
-  localStorage.setItem('uName', b);
+  //create LS key with value of 'fieldValue'
+  localStorage.setItem('uName', fieldValue);
   //automatically take user to page two.html
   location.href = "two.html";
 }
@@ -18,5 +18,12 @@ function getName() {
 function getUser() {
   var c = localStorage.getItem('uName');
   document.getElementById('placeholder').innerHTML = "Hello, " + c;
+} */
+
+function saveName() {
+  var fieldValue = document.getElementById('textfield').value;
+  localStorage.setItem('userName', fieldValue)'
+  window.location.href = 'two.html';
 }
 
+  
